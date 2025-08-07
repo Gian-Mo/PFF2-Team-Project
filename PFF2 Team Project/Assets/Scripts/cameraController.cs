58770c6cfcafe759d1020bba4198f6f49d4f2870
Gian-Mo <gmr.11.sc@gmail.com>
2025-08-07 17:36:40 -0400

    Camera Control Works

---
+++ PFF2 Team Project/Assets/Scripts/cameraController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    [SerializeField] int sens;
+    [SerializeField] float lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    float rotX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+
+        //Use invert Y option
+
+        if (invertY)
+        {
+            rotX += mouseY;
+        }
+        else { 
+            rotX -= mouseY;
+        }
+
+
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        //Rotate camera to look up and down 
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+
+        //Rotate the player to look left and right 
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
